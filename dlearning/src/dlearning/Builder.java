9f2473dbfeb51cdfd0db47f2abd2a7066c6a9722
Mauricio <impendingfern@gmail.com>
2018-04-07 18:57:21 -0500

    Crear archivo de output de dlv

--- dlearning/src/dlearning/Builder.java
+++ dlearning/src/dlearning/Builder.java
@@ -7,6 +7,7 @@ package dlearning;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.ProcessBuilder;
 /**
  *
@@ -20,11 +21,13 @@ public class Builder {
     public static void watch(final Process process) {
     new Thread(){
         public void run() {
+            dlearning.Archivo output = new dlearning.Archivo("plan_output");
             BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line = null; 
             try {
                 while ((line = input.readLine()) != null) {
                     System.out.println(line);
+                    output.write(line);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -35,12 +38,16 @@ public class Builder {
     
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        ProcessBuilder builder = new ProcessBuilder("/home/mau/Tools/NetBeansProjects/dlearning/src/dlearning/dlv.x86-64-linux-elf-static.bin", "-help");
+        ProcessBuilder builder = new ProcessBuilder("/Users/pamelagarcia/Downloads/dlv.i386-apple-darwin.bin", "-FP", "/Users/pamelagarcia/DLearning/dlearning.dl", "/Users/pamelagarcia/DLearning/dlearning.plan");
         builder.redirectErrorStream(true);
         final Process process = builder.start();
-
-        Builder.watch(process);
         
+        PrintWriter input = new PrintWriter(process.getOutputStream()); 
+        input.print("y");
+        input.print("y");
+        input.close();
+        
+        Builder.watch(process);
     }
     
 }
