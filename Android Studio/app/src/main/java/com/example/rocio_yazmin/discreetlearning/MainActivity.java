d6a7606026e654c6554a0dc01a98bdd2435bb8ba
MauAraujo <impendingfern@gmail.com>
2018-04-24 23:52:51 -0500

    Agregar proyecto de Android Studio

---
+++ Android Studio/app/src/main/java/com/example/rocio_yazmin/discreetlearning/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rocio_yazmin.discreetlearning;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.rocio_yazmin.discreetlearning.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+
+
+}
---
+++ Android Studio/app/src/main/java/com/example/rocio_yazmin/discreetlearning/ini.java
@@ -0,0 +1,14 @@
+package com.example.rocio_yazmin.discreetlearning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ini extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ini);
+    }
+
+}
