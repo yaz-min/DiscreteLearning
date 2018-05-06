49bea928cac7b4d7b1c3fc4a781927c2445cc9f1
camielias <37197609+camielias@users.noreply.github.com>
2018-05-06 16:04:08 -0500

    Add files via upload

---
+++ DiscreetLearning/src/discreetlearning/DiscreetLearning.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discreetlearning;
+
+/**
+ *
+ * @author Camila
+ */
+public class DiscreetLearning {
+    
+    public static String user="";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Login login = new Login();
+        login.setVisible(true);
+    }
+    
+}
